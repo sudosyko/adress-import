0a34a677cf8b2e8544e182cfb3fe575bf5f87605
miguel montag <miguel.montag@outlook.com>
2024-11-29 11:32:10 +0100

    Add project files.

---
+++ adress-import/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace adress_import
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
