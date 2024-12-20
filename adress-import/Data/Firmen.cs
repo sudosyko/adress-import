cfd220a288842d329a76269fe9f75fcd8d703761
LeoPieren <pieren.leo@gmail.com>
2024-12-20 17:26:52 +0100

    Tabellen Klassen und Context erstellt

---
+++ adress-import/Data/Firmen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adress_import.Data
+{
+    internal class Firmen
+    {
+        public int FID { get; set; }
+        public string Firmenname { get; set; }
+        public ICollection<Adressen> Adressen { get; set; }
+    }
+}
---
+++ adress-import/Data/Ortschaften.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adress_import.Data
+{
+    internal class Ortschaften
+    {
+        public int OID { get; set; }
+        public string Ort { get; set; }
+        public int Postleitzahl { get; set; }
+        public ICollection<Adressen> Adressen { get; set; }
+    }
+}
