c274b19d36ca74410a8258667ff65cfe8b495ed7
LeoPieren <pleger@Daskalos.>
2024-12-20 16:39:14 +0100

    DB Connector klasse erstellt, Ordnerstruktur angepasst auf 3 schichten architektur

---
+++ adress-import/Service/DbConnector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adress_import.Service
+{
+    internal class DbConnector
+    {
+    }
+}
